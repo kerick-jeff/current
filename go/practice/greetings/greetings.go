cc14e421c9f4842ca03cce69d60f6a77216a48a4
k3r!ck <frukerickjeff@gmail.com>
2026-03-30 16:06:45 +0100

    go: added error handling

--- go/practice/greetings/greetings.go
+++ go/practice/greetings/greetings.go
@@ -1,10 +1,19 @@
 package greetings
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
 // Hello returns a greeting for the named person.
-func Hello(name string) string {
-	// Return a greeting that embeds the name in a message.
-	message := fmt.Sprintf("Hi, %v! Welcome.", name)
-	return message
+func Hello(name string) (string, error) {
+	var message string
+
+	if name == "" {
+		message = name
+		return message, errors.New("empty name")
+	} else { // Return a greeting that embeds the name in a message.
+		message = fmt.Sprintf("Hi, %v! Welcome.", name)
+		return message, nil
+	}
 }
--- go/practice/hello/hello.go
+++ go/practice/hello/hello.go
@@ -2,10 +2,22 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"practice/greetings"
 )
 
 func main() {
-	greeting := greetings.Hello("Gladys")
-	fmt.Println(greeting)
+	// Set properties of the predefined Logger, including
+	// the log entry prefix and a flag to disable printing
+	// the time, source file, and line number.
+	log.SetPrefix("greetings: ")
+	log.SetFlags(0)
+
+	greeting, err := greetings.Hello("")
+
+	if err == nil { // If no error was returned, print the returned message to the console.
+		fmt.Println(greeting)
+	} else { // If an error was returned, print it to the console and exit the program.
+		log.Fatal(err)
+	}
 }
